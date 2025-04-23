f00651ed75a064b09563736c08f53ab890a0db2f
qwerty-uiop-qaz <qwertyuiop@webname.com>
2025-04-23 16:44:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.get("/join.htm", (req, res) => 
-{
-    const filePath = path.join(__dirname, 'files', 'join.htm');
-    res.type('html').send(join);
-});
+app.get("/join.htm", (req, res) => res.type('html').send(join));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
