aeb1debc7c674f5f7490331ce4ae73e861e46e06
qwerty-uiop-qaz <qwertyuiop@webname.com>
2025-04-23 16:32:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ app.get("join.htm", (req, res) =>
     });
 });
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
-});
-
-
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
